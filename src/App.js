065fa4c3563be5dae05cf166840c86c621c12f26
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-30 17:01:46 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import JeuDe from './components/JeuDe';
 
 function App() {
+  const val = Math.round(Math.random()*5 + 1)
   return (
     <div className="container">
-     <JeuDe cache={2}/>
+     <JeuDe cache={val}/>
     </div>
   );
 }
--- src/components/JeuDe.jsx
+++ src/components/JeuDe.jsx
@@ -4,10 +4,19 @@ class JeuDe extends Component {
   constructor(props) {
     super(props);
     this.state = { face: null, compteur: 0, fin: false };
+
+    this.jouer = this.jouer.bind(this);
+    this.initialiser = this.initialiser.bind(this);
   }
   jouer() {
     const valeur = Math.floor(Math.random() * 6) + 1;
-    //completer le code
+    let fin = false;
+    if (this.props.cache == valeur) fin = true;
+    this.setState({
+      face: valeur,
+      compteur: this.state.compteur + 1,
+      fin: fin,
+    });
   }
   initialiser() {
     this.setState({ face: null, compteur: 0, fin: false });
@@ -15,13 +24,18 @@ class JeuDe extends Component {
   render() {
     return (
       <div className="jeude">
-        <img src={require('../images/jeuDe.png')} />
+        <img src={require("../images/jeuDe.png")} />
         <h1>Jeu de Dé</h1>
-        <h2>face:….. </h2>
-        <h2>nombre d'essais…..</h2>
-        <button onClick={() => this.jouer()}>jouer</button>
-        <p>Bravo vous avez trouvez la face cachée.....</p>
-        <button onClick={() => this.initialiser()}>Initialiser</button>
+        <h2>face: {this.state.face} </h2>
+        <h2>nombre d'essais {this.state.compteur}</h2>
+        {!this.state.fin ? (
+          <button onClick={() => this.jouer()}>jouer</button>
+        ) : (
+          <div>
+            <p>Bravo vous avez trouvez la face cachée.....</p>
+            <button onClick={() => this.initialiser()}>Initialiser</button>
+          </div>
+        )}
       </div>
     );
   }
